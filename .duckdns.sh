95c2b6b97c900643869a52ccca315a007212e4a8
Warren Eaton <warren.eaton.sciaccaluga@gmail.com>
2019-05-20 14:29:01 -0400

    add duckdns script

---
+++ .duckdns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+logger -t DuckDNS "Updating DuckDNS entries"
+EXITCODE=0
+DUCK_DOMAIN=''
+DUCK_TOKEN=''
+OUTPUT=$(curl -k -s "https://www.duckdns.org/update?domains=${DUCK_DOMAIN}&token=${DUCK_TOKEN}&ip=")
+logger -t DuckDNS ${OUTPUT}
+if [ "${OUTPUT}" == "KO" ]; then
+	logger -t DuckDNS "You should check if your domain/token is correct because the server responded negatively!"
+	EXITCODE=1
+fi
+exit $EXITCODE
